3c745c950f3632d5d1e75c047f3ddf8f1f6c937b
Filipe Lopes <filiperocklopes@gmail.com>
2024-07-16 09:30:50 -0300

    feat(wip): add edit page

--- web/src/views/Edit/index.jsx
+++ web/src/views/Edit/index.jsx
@@ -1,3 +1,37 @@
-export default () => {
-    return <p>Formulário de edição</p>
+import Input from "src/components/Input"
+import Select from "src/components/Select"
+import Button from "src/components/Button"
+
+const EditForm = () => {
+    const formik = useFormik({
+        initialValues: {},
+        onSubmit: (values) => {
+            console.log(values)
+        }
+    })
+    return <article>
+        <h2>Formulário de Edição</h2>
+        <form onSubmit={formik.handleSubmit}>
+            <Input
+                type="text"
+                name="specie.name"
+                formik={formik}
+            />
+            <h3>Nomes Populares</h3>
+            <Select
+                formik={formik}
+                name="popularNames"
+                options={[]}
+                creatable
+                isMulti
+                />
+                <h4>Descrições</h4>
+                <h3>Metabólitos secundários</h3>
+                <h4>Sintomas relacionados</h4>
+                <h3>Efeitos Terapêuticos</h3>
+            <Button type="submit">Salvar</Button>
+        </form>
+    </article>
 }
+
+export default EditForm
