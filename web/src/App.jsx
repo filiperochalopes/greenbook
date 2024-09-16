974c42194676fb080d3eecbc80e7008137194347
Filipe Lopes <filiperocklopes@gmail.com>
2024-09-16 16:50:08 -0300

    feat: add notistack to better client feedback on copy paste

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import SearchResult from './components/SearchResult'
+import Search from './views/Search'
 import Content from './components/Content'
 import AppContext from './services/context'
 import { GlobalStyle, theme } from './styles'
@@ -10,6 +8,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Edit from './views/Edit'
 import Add from './views/Add'
+import { SnackbarProvider } from 'notistack';
 
 const client = new ApolloClient({
   // uri: 'https://greenbook.filipelopes.med.br/graphql',
@@ -32,24 +31,18 @@ function App() {
     <AppContext.Provider value={{ searchResults, setSearchResults, individualResult, setIndividualResult, loading, setLoading, searchTerm, setSearchTerm }}>
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
+        <SnackbarProvider dense={true} anchorOrigin={{ vertical: 'top', horizontal: 'center' }} autoHideDuration={3000}>
           <GlobalStyle />
           <Router>
             <Routes>
-              <Route exact path="/" element={<>
-                {loading && <div className="loading-bar"></div>}
-                <Header withLogo={Boolean(!searchResults.length && !individualResult.title && !loading)} />
-                {/* Mostra o resultado da busca */}
-                {Boolean(searchResults.length && !individualResult.title) && <SearchResult />}
-                {/* Retorna vazio caso não encontre nada */}
-                {Boolean(!searchResults.length && !loading && searchTerm && !individualResult.title) && <center style={{ opacity: 0.5, margin: '2rem 0' }}>Não foram encontrados resultados para sua busca</center>}
-                <Footer withLogo={Boolean(searchResults.length || individualResult.title)} />
-              </>} />
+              <Route exact path="/" element={<Search/>} />
               <Route exact path="/edit" element={<Edit />} />
               <Route exact path="/add" element={<Add />} />
               {/* Rota dinâmica para espécies e efeitos terapêuticos */}
               <Route exact path="/:type/:id" element={<Content />} />
             </Routes>
           </Router>
+          </SnackbarProvider>
         </ThemeProvider>
       </ApolloProvider>
     </AppContext.Provider>
--- web/src/components/Content/index.jsx
+++ web/src/components/Content/index.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import PageTemplate from "../PageTemplate";
 import SearchResult from "src/components/SearchResult";
+import { enqueueSnackbar } from 'notistack';
 
 const Content = () => {
   const { individualResult: r, setIndividualResult, searchResults, searchTerm, loading } = useContext(AppContext),
@@ -46,54 +47,60 @@ const Content = () => {
   }, [searchTerm])
 
   return <PageTemplate>
-      {Boolean(searchResults.length > 0 && searchTerm !== "" && !loading) ? <SearchResult /> : <Article>
-        <section style={{textAlign: "center", marginTop: "1rem"}}>
-          <button onClick={() => navigator.clipboard.writeText(window.location.href)}>
-            Compartilhar
-          </button>
-        </section>
-        
-        <section>
-          <header>
-            <h1>{r.title}</h1>
-            {r.description && <p>{r.description}</p>}
-          </header>
-        </section>
-        {Boolean(r.species?.length) && <section>
-          <header>
-            <h2>Especies</h2>
-          </header>
-          {r.species.map((elem) => <Link to={handleItemClick(elem.q)} key={elem.name}><button>{elem.name}</button></Link>)}</section>}
-        {Boolean(r.popularNames?.length) && <section>
-          <header>
-            <h2>Nomes Populares</h2>
-          </header>
-          {r.popularNames.map((elem) => <Link to={handleItemClick(elem.q)} key={elem.name}><button>{elem.name}</button></Link>)}
-        </section>}
-        {Boolean(r.metabolites?.length) && <section>
-          <header>
-            <h2>Metabólitos</h2>
-          </header>
-          {r.metabolites.map((elem) => <button onClick={() => handleItemClick(elem.q)} key={elem.name}>{elem.name}</button>)}
-        </section>}
-        {Boolean(r.therapeuticEffects?.length) && <section>
-          <header>
-            <h2>Efeitos terapêuticos</h2>
-          </header>
-          {r.therapeuticEffects.map((elem) => <button onClick={() => handleItemClick(elem.q)} key={elem.term}>{elem.term}</button>)}
-        </section>}
-        {Boolean(r.prescriptionSuggestions?.length) && <section>
-          <header>
-            <h2>Sugestões de Prescricão</h2>
-          </header>
-          <i>Clique em uma das prescrições abaixo para copiar:</i>
-          <ul>
-            {r.prescriptionSuggestions.map((elem, index) =>
-              <li key={index}><button onClick={handleCopyToClipboard}>{elem.specie.name} ({elem.part.name}) {elem.dosage}</button>{(elem.description || elem.quantity) && <p>{elem.quantity && <span>{elem.quantity}</span>}{elem.description && elem.quantity && <span> - </span>}{elem.description && <span>{elem.description}</span>}</p>}</li>)}
-          </ul>
-        </section>}
-      </Article>}
-      </PageTemplate>
+    {Boolean(searchResults.length > 0 && searchTerm !== "" && !loading) ? <SearchResult /> : <Article>
+      <section style={{ textAlign: "center", marginTop: "1rem" }}>
+        <button onClick={() => {
+          navigator.clipboard.writeText(window.location.href)
+          enqueueSnackbar("Link copiado", { variant: "success" })
+        }}>
+          Compartilhar
+        </button>
+      </section>
+
+      <section>
+        <header>
+          <h1>{r.title}</h1>
+          {r.description && <p>{r.description}</p>}
+        </header>
+      </section>
+      {Boolean(r.species?.length) && <section>
+        <header>
+          <h2>Especies</h2>
+        </header>
+        {r.species.map((elem) => <Link to={handleItemClick(elem.q)} key={elem.name}><button>{elem.name}</button></Link>)}</section>}
+      {Boolean(r.popularNames?.length) && <section>
+        <header>
+          <h2>Nomes Populares</h2>
+        </header>
+        {r.popularNames.map((elem) => <Link to={handleItemClick(elem.q)} key={elem.name}><button>{elem.name}</button></Link>)}
+      </section>}
+      {Boolean(r.metabolites?.length) && <section>
+        <header>
+          <h2>Metabólitos</h2>
+        </header>
+        {r.metabolites.map((elem) => <button onClick={() => handleItemClick(elem.q)} key={elem.name}>{elem.name}</button>)}
+      </section>}
+      {Boolean(r.therapeuticEffects?.length) && <section>
+        <header>
+          <h2>Efeitos terapêuticos</h2>
+        </header>
+        {r.therapeuticEffects.map((elem) => <button onClick={() => handleItemClick(elem.q)} key={elem.term}>{elem.term}</button>)}
+      </section>}
+      {Boolean(r.prescriptionSuggestions?.length) && <section>
+        <header>
+          <h2>Sugestões de Prescricão</h2>
+        </header>
+        <i>Clique em uma das prescrições abaixo para copiar:</i>
+        <ul>
+          {r.prescriptionSuggestions.map((elem, index) =>
+            <li key={index}><button onClick={(e) => {
+              handleCopyToClipboard(e)
+              enqueueSnackbar("Prescrição copiada", { variant: "success" })
+            }}>{elem.specie.name} ({elem.part.name}) {elem.dosage}</button>{(elem.description || elem.quantity) && <p>{elem.quantity && <span>{elem.quantity}</span>}{elem.description && elem.quantity && <span> - </span>}{elem.description && <span>{elem.description}</span>}</p>}</li>)}
+        </ul>
+      </section>}
+    </Article>}
+  </PageTemplate>
 }
 
 export default Content
